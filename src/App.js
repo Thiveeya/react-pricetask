956fb498433d34c64a91a02168a05ca7fcdcaa17
Thiveeya <rbsthivi97@gmail.com>
2023-06-13 21:23:24 +0530

    react deploy

--- src/App.js
+++ src/App.js
@@ -1,40 +1,4 @@
-// import logo from './logo.svg';
-// import './App.css';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-// const App=()=>
-// {
-//   console.log('Hello from Component');
-//   return(
-//     <div>
-//       <p>
-//         Hello World
-//       </p>
-//     </div>
-//   )
-// }
-
-
-// export default App;
 import "./App.css";
 
 function App() {
@@ -127,3 +91,5 @@ function Details(props) {
     </div>
   );
 }
+
+
